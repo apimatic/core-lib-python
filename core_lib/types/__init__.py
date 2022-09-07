3f0359156e7132cad8c5a080a11244c98b066944
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2022-09-07 18:15:32 +0500

    Merge Conflicts resolved

--- core_lib/types/__init__.py
+++ core_lib/types/__init__.py
@@ -2,9 +2,6 @@ __all__ = [
     'datetime_format',
     'parameter',
     'error_case',
-    'file_wrapper'
-<<<<<<< HEAD
+    'file_wrapper',
     'auths',
-=======
->>>>>>> 6fffa4b21284d8ae47e2216e195fbb10f65d6f3f
 ]
