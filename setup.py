a0da86fdce54912b334f91ab476d8fbbcaf44772
Muhammad Sufyan <sufyankhanrao123@gmail.com>
2023-05-25 13:13:37 +0500

    requests version upgraded to 2.31.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'apimatic-core-interfaces~=0.1.0',
         'jsonpickle~=3.0.1, >= 3.0.1',
         'python-dateutil~=2.8.1',
-        'requests~=2.28.1',
+        'requests~=2.31',
         'enum34~=1.1, >=1.1.10',
         'setuptools~=66.0.0',
         'jsonpointer~=2.3'
