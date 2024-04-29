fe556a4d32b664eb2435a0b110b246abbd14f8c0
Muhammad Sufyan <sufyankhanrao123@gmail.com>
2024-04-29 20:47:02 +0500

    reverts core interfaces dependency change

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     url='https://github.com/apimatic/core-lib-python',
     packages=find_packages(),
     install_requires=[
-        'git+https://github.com/apimatic/core-interfaces-python@28-add-logging-support',
+        'apimatic-core-interfaces~=0.1.0',
         'jsonpickle~=3.0.1, >= 3.0.1',
         'python-dateutil~=2.8.1',
         'requests~=2.31',
