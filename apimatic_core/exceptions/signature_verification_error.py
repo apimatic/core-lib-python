fc99e6bdfb2103e0a3138a0e9a8ea9d3fd2fb796
DESKTOP-10GD4VQ\Sufyan <muhammad.sufyan@apimatic.io>
2025-08-27 20:07:55 +0500

    override str method for custom exception

--- apimatic_core/exceptions/signature_verification_error.py
+++ apimatic_core/exceptions/signature_verification_error.py
@@ -9,3 +9,6 @@ class SignatureVerificationError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
